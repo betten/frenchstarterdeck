37d048b0e99926957e57bee0f543250cbec8fddf
betten <betten@bc.edu>
2013-03-26 08:30:58 -0400

    switching to tab separated file

--- web.rb
+++ web.rb
@@ -8,5 +8,5 @@ end
 
 post '/generate' do
   attachment "frenchstarterdeck.txt"
-  params[:word].map { |w| "#{w[1]["word"]},#{w[1]["french"]},#{w[1]["english"]}\r\n" unless w[1].nil? }
+  params[:word].map { |w| "#{w[1]["word"]}\t#{w[1]["french"]}\t#{w[1]["english"]}\r\n" unless w[1].nil? }
 end
